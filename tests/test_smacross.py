55bfbfb806d215d9eddf0f1cbd0fd1eb87e7e1f1
You Xie <xyshell@bu.edu>
2025-03-29 16:08:04 -0400

    add assert_report

--- tests/test_smacross.py
+++ tests/test_smacross.py
@@ -1,6 +1,6 @@
 from tradingbot import Bot
 from tradingbot.strategy.smacross import SMACross, SMACrossFuture
-from tradingbot.util import hash_pd
+from . import assert_report
 
 
 def test_smacross_backtest(snapshot):
@@ -9,19 +9,7 @@ def test_smacross_backtest(snapshot):
     bot.run()
 
     # report
-    report = bot.strategy.report
-    assert len(report['asset']) == len(report['portfolio']) == snapshot  # timeline count
-    assert report['asset'].iloc[-1].drop("timestamp").mean().round(4) == snapshot  # ending asset
-    assert report['portfolio'].iloc[-1].drop("timestamp").mean().round(4) == snapshot  # ending nav
-    assert report['transaction']['timestamp'].mean() == snapshot  # historical transaction
-    assert report['trade']['duration'].mean() == snapshot  # historical trade
-    assert report['order']['amount'].round(4).mean() == snapshot  # historical order
-    assert hash_pd(report['order'].set_index("created_at")['action']) == snapshot
-    assert report['order']['filled_at'].mean() == snapshot  # historical order
-    # selected agg metrics
-    assert report['summary']["trade_num"] == snapshot
-    assert report['summary']["ir"].round(4) == snapshot
-    assert report['summary']["expectancy"].round(4) == snapshot
+    assert_report(bot.strategy.report, snapshot)
 
     # plot
     fig = bot.strategy.plot()
